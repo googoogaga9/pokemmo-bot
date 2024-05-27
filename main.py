ebb2ba50ca83952cff3ca41e6db3d3ea1e836032
Taj Bishop <bishoptaj@hotmail.com>
2024-05-27 21:58:53 +0800

    changes

--- main.py
+++ main.py
@@ -23,56 +23,34 @@ def wait(min_wait=0, max_wait=10):
     """Wait a random number of seconds between min_wait and max_wait."""
     time.sleep(random_delay(min_wait, max_wait))
 
+def move(direction_key, direction_char, spaces=1):
+    """Move in a specified direction a certain number of spaces."""
+    global DIRECTION_FACING
+    turn_time = TIME_TO_TURN if DIRECTION_FACING != direction_char else 0
+    pyautogui.keyDown(direction_key)
+    time.sleep(TIME_PER_SPACE * spaces + random_delay(-0.02, 0.02) + turn_time)
+    pyautogui.keyUp(direction_key)
+    time.sleep(random_delay(0.1, 0.2))  # Small randomized delay after moving
+    DIRECTION_FACING = direction_char
+
 def move_left(spaces=1):
     """Move left a certain number of spaces."""
-    global DIRECTION_FACING
-    turn_time = TIME_TO_TURN
-    if DIRECTION_FACING == 'L':
-        turn_time = 0
-    pyautogui.keyDown(LEFT_KEY)
-    time.sleep(TIME_PER_SPACE * spaces + random_delay(-0.02, 0.02) + turn_time)
-    pyautogui.keyUp(LEFT_KEY)
-    time.sleep(random_delay(0.1, 0.2))  # Small randomized delay after moving
-    DIRECTION_FACING = 'L'
+    move(LEFT_KEY, 'L', spaces)
 
 def move_right(spaces=1):
     """Move right a certain number of spaces."""
-    global DIRECTION_FACING
-    turn_time = TIME_TO_TURN
-    if DIRECTION_FACING == 'R':
-        turn_time = 0
-    pyautogui.keyDown(RIGHT_KEY)
-    time.sleep(TIME_PER_SPACE * spaces + random_delay(-0.02, 0.02) + turn_time)
-    pyautogui.keyUp(RIGHT_KEY)
-    time.sleep(random_delay(0.1, 0.2))  # Small randomized delay after moving
-    DIRECTION_FACING = 'R'
+    move(RIGHT_KEY, 'R', spaces)
 
 def move_up(spaces=1):
     """Move up a certain number of spaces."""
-    global DIRECTION_FACING
-    turn_time = TIME_TO_TURN
-    if DIRECTION_FACING == 'U':
-        turn_time = 0
-    pyautogui.keyDown(UP_KEY)
-    time.sleep(TIME_PER_SPACE * spaces + random_delay(-0.02, 0.02) + turn_time)
-    pyautogui.keyUp(UP_KEY)
-    time.sleep(random_delay(0.1, 0.2))  # Small randomized delay after moving
-    DIRECTION_FACING = 'U'
+    move(UP_KEY, 'U', spaces)
 
 def move_down(spaces=1):
     """Move down a certain number of spaces."""
-    global DIRECTION_FACING
-    turn_time = TIME_TO_TURN
-    if DIRECTION_FACING == 'D':
-        turn_time = 0
-    pyautogui.keyDown(DOWN_KEY)
-    time.sleep(TIME_PER_SPACE * spaces + random_delay(-0.02, 0.02) + turn_time)
-    pyautogui.keyUp(DOWN_KEY)
-    time.sleep(random_delay(0.1, 0.2))  # Small randomized delay after moving
-    DIRECTION_FACING = 'D'
+    move(DOWN_KEY, 'D', spaces)
 
 def run_back_and_forth():
-    """Randomly run back and forth continuously staying within 3 spaces of the original position"""
+    """Randomly run back and forth continuously staying within 3 spaces of the original position."""
     step_right = 0
     while True:
         step_left = secrets.choice([0, 1, 2, 3])
@@ -81,7 +59,7 @@ def run_back_and_forth():
         move_right(step_right + step_left)
 
 def follow_path():
-    """Run between the PC and the PokeMart in Viridian City"""
+    """Run between the PC and the PokeMart in Viridian City."""
     while True:
         dx1 = secrets.choice([-1, 0, 0, 0, 1])
         dy1 = secrets.choice([-1, 0, 0, 0, 1])
@@ -91,7 +69,7 @@ def follow_path():
         move_up(6 + dy1)
         move_right(5 - dx1)
         move_up(2 - dy1)
-        wait(2,2.5)
+        wait(2, 2.5)
         move_up(4)
 
         dx2 = secrets.choice([-1, 0, 0, 0, 1])
@@ -102,42 +80,37 @@ def follow_path():
         move_down(8 + dy2)
         move_left(5 - dx2)
         move_up(2 - dy2)
-        wait(2,2.5)
+        wait(2, 2.5)
         move_up(4)
-
-def follow_up():
-    move_up(1)
-    wait(2, 2.5)
-    move_up(4)
 
 def main_menu():
     """Display the main menu and prompt the user to select an option."""
-    print("Select an option:")
-    print("1. Run back and forth")
-    print("2. Follow path")
-    print("3. Exit")
+    options = {
+        "1": ("Run back and forth", run_back_and_forth),
+        "2": ("Follow path", follow_path),
+        "3": ("Exit", None)
+    }
 
-    choice = input("Enter your choice: ")
+    while True:
+        print("Select an option:")
+        for key, (description, _) in options.items():
+            print(f"{key}. {description}")
 
-    if choice == '1':
-        print("Starting the back-and-forth bot in 5 seconds. Press Ctrl+C to stop.")
-        time.sleep(5)
-        try:
-            run_back_and_forth()
-        except KeyboardInterrupt:
-            print("Bot stopped by user.")
-    elif choice == '2':
-        print("Following the specified path in 5 seconds. Press Ctrl+C to stop.")
-        time.sleep(5)
-        try:
-            follow_path()
-        except KeyboardInterrupt:
-            print("Path following stopped by user.")
-    elif choice == '3':
-        print("Exiting the program.")
-    else:
-        print("Invalid choice. Please try again.")
-        main_menu()
+        choice = input("Enter your choice: ")
+
+        if choice in options:
+            if choice == "3":
+                print("Exiting the program.")
+                break
+
+            print(f"Starting '{options[choice][0]}' in 5 seconds. Press Ctrl+C to stop.")
+            time.sleep(5)
+            try:
+                options[choice][1]()
+            except KeyboardInterrupt:
+                print(f"{options[choice][0]} stopped by user.")
+        else:
+            print("Invalid choice. Please try again.")
 
 if __name__ == "__main__":
     main_menu()
